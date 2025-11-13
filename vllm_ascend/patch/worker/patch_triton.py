9b0cd6af484f7a9e9250082d76ec7be940e540ef
shiyuan680 <917935075@qq.com>
2025-11-13 11:37:55 +0800

    update chunk_gated_delta_rule

--- vllm_ascend/patch/worker/patch_triton.py
+++ vllm_ascend/patch/worker/patch_triton.py
@@ -5,12 +5,13 @@ import vllm.model_executor.layers.mamba.ops.causal_conv1d
 
 from vllm_ascend.ops.casual_conv1d import (causal_conv1d_fn,
                                            causal_conv1d_update_npu)
-from vllm_ascend.ops.fla import LayerNormFn, torch_chunk_gated_delta_rule
+from vllm_ascend.ops.fla import LayerNormFn
 from vllm_ascend.ops.sigmoid_gating import \
     fused_recurrent_gated_delta_rule_fwd_kernel
+from vllm_ascend.ops.triton.chunk import chunk_gated_delta_rule
 
 vllm.model_executor.layers.mamba.ops.causal_conv1d.causal_conv1d_update = causal_conv1d_update_npu
 vllm.model_executor.layers.mamba.ops.causal_conv1d.causal_conv1d_fn = causal_conv1d_fn
 vllm.model_executor.layers.fla.ops.fused_recurrent.fused_recurrent_gated_delta_rule_fwd_kernel = fused_recurrent_gated_delta_rule_fwd_kernel
 vllm.model_executor.layers.fla.ops.layernorm_guard.LayerNormFn = LayerNormFn
-vllm.model_executor.layers.fla.ops.chunk.chunk_gated_delta_rule = torch_chunk_gated_delta_rule
+vllm.model_executor.layers.fla.ops.chunk.chunk_gated_delta_rule = chunk_gated_delta_rule
